4c1b4e583575c2e52b919e7d391ff1d55cfaffbf
pandur <pandur>
2000-09-21 15:35:48 +0000

    Initial revision

---
+++ htdocs/index.php
@@ -0,0 +1,73 @@
+<html>
+<title>Morgana</title>
+<body bgcolor="white">
+
+<? require "menu.start" ?>
+
+
+<h1>The Morgana Project</h1>
+
+<h2>What is Morgana</h2>
+
+Morgana will be an Artifical Intelligence Simulator. Especially in the
+field of so called "soft computing" - which means neural networks, fuzzy
+controls and genetic algorithms. Yet Morgana targets neural networks,
+fuzzy controls will follow.<p>
+
+The concept behind that is "the connection machine" which represents all
+entities as objects which can be connected to transfer single values
+between them. That's the way neural networks work, but I think fuzzy
+controls can be accessed the same way. 
+
+
+<h2>State of the Project</h2>
+
+Morgana startet as my diploma thesis, but soon it will be time to go
+open.<p>
+
+Currently I prepare for the official start of it. [aimed date: 1sep2000]
+This means to clean up and bautify about 8000 lines of code and tons of
+scripts and documentation. As soon as I reached this goal I'll upload the
+sources to the CVS-Server. [aimed date for CVS: mid of august]<p>
+
+In means of functionality: Morgana has much of code and even more drafts
+and design (most of it must yet be written down), but only few
+functionality. If you enjoy moving around blue boxes with black lines
+between them you may have much fun with it, but for those who want to see
+real networks: sorry it needs much time and development till Morgana can
+satisfy you.
+
+<h2>Accessable Ressources</h2>
+
+<a href="http://www.htw-dresden.de/~htw6966/diplom">Original diploma pages
+(mostly German)</a><p>
+
+Maillists:
+
+<ul>
+
+<li>Developers and (yet) open discussions list:<br>
+<tt>morgana-devel@lists.sourceforge.net</tt><br>
+(<a href="http://www.geocrawler.com/redir-sf.php3?list=morgana-devel">Archive</a>| 
+ <a href="http://lists.sourceforge.net/mailman/listinfo/morgana-devel">Subscription</a>)<p>
+
+</ul>
+
+CVS: yet not established.<p>
+
+<a href="http://sourceforge.net/projects/morgana">Project Info</a><p>
+
+If you think more ressources may be necessary: 
+<a href="mailto:pandur@users.sourceforge.net">contact me</a>.
+
+<h2>The name</h2>
+
+Morgana was the name Robert N. Charrette gave to the AI in the
+(first) ShadowRun trilogy.<p>
+
+[<a href="srhints.html">Some hints for those who are familiar with
+it.</a>]
+
+<? require "menu.end" ?>
+
+</html>
